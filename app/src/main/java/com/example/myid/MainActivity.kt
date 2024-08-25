49ce804d1aa6c31e7c5713dba02197fa6ef19289
Rian <rfrian111@gamil.com>
2024-08-25 21:48:58 +0600

    First Application

---
+++ app/src/main/java/com/example/myid/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myid
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btn1 = binding.btn
+        btn1.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Submission Successful", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
